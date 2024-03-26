e6f5f38f087ece1567890c43adfb6ac641e13a5b
nadyasri <nadyasriandriani@gmail.com>
2024-03-27 03:57:11 +0700

    Tugas IAE nadya

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">La Liga</a>
+    <a class="navbar-brand" href="/">La Liga</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link" aria-current="page" href="/standing">Standing</a>
+        <a class="nav-link" aria-current="page" href="/">Standing</a>
         <a class="nav-link" href="/player">Players</a>
       </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\playerController;
 use App\Http\Controllers\standingController;
 use App\Http\Controllers\playerDataController;
 
-Route::get('/standing', [standingController::class, 'standing']);
+Route::get('/', [standingController::class, 'standing']);
 Route::get('/player', [playerController::class, 'player']);
 Route::get('dataPlayer/{id}', [playerDataController::class, 'playerData'])->name('dataPlayer');
